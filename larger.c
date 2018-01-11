b2d740f801b533421d243908216d8acc5ca5ac7e
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-01-11 16:07:24 +0530

    Create larger.c

---
+++ larger.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,num3;
+printf("enter the number num1,num2,num3);
+scanf("%d",%d","%d",&num1,&num2,&num3);
+if(num1 > num2)
+{
+if(num1 >num3)
+{
+printf("num1 is largest ");
+}
+else
+{
+printf("num3 is largest ");
+}
+else if (num2 > num3)
+{
+printf("num2 is largest");
+}
+else
+{
+printf("num3 is largest");
+}
