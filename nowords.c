5585a9fab0ca03bb3a604245adf846a277b1e7ae
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-01-21 10:58:07 +0530

    Create nowords.c

---
+++ nowords.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int c=0,i,n;
+printf("Enter  value of character");
+scanf("%c",&a);
+n=strlen(a);
+for(i=0;i<=n;i++)
+{
+if(a[i]!=" ")
+c++;
+}
+printf("no.of words %d",c+1);
+getch();
+}
