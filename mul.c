a90721f0255099d588dfef08c59f4e8e0b63ba58
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-01-21 09:12:31 +0530

    Create mul.c

---
+++ mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i;
+	printf("enter any number");
+	scanf("%d",&n);
+	for(i=1;i<=5;i++)
+	 printf("First five multipliers are given as:", n*i);
+	return 0;
+}
