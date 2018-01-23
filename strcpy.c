47f845a48ac00f00d6d76dbef266ace94e0d1118
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-01-23 15:43:21 +0530

    Create strcpy.c

---
+++ strcpy.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+char st[10],st1[10];
+scanf("%s",&st);
+scanf("%s",&st1);
+i=0;
+while((st[i]==st1[i] && st[i]!='\0'))
+i++;
+if(st[i]<st1[i])
+printf("%s",st1);
+else if(st[i]>st1[i])
+printf("%s",st);
+else
+printf("%s",st);
+return;
+}
