8d1d254679ef3c601003c229291c4256a0cd6048
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-01-25 16:03:07 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int  main()
+{
+int m,n;char a[20],b[20];
+
+	
+	printf("enter the value of a and b");
+	scanf("%s %s",&a,&b);
+	m=strlen(a);
+	n=strlen(b);
+	if(m>=n)
+	{
+		printf("%s",a);
+	}
+	else
+	{
+		printf("%s",b);
+	}
+	
+	return 0;
+}
