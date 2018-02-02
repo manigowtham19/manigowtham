b4cd880c2f2ce5288660a1e4b0f1ea30b41de23b
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-02-02 14:15:01 +0530

    Update words.c

--- words.c
+++ words.c
@@ -6,7 +6,7 @@ void main()
     char s[200];
     int count = 0, i;
  
-    printf("enter the string\n");
+    printf("enter any string\n");
     scanf("%s", s);
     for (i = 0;s[i] != '\0';i++)
     {
