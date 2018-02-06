923cbcca3667cbb87f25edf0c8ebe874b021922a
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-02-06 11:02:44 +0000

    Update nearpow.c

--- nearpow.c
+++ nearpow.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-	int n,i,c=0,m=1,p=2;
+	int n,i,c=0,n=1,p=2;
 	scanf("%d",&n);
 	while(n!=1)
 	{
@@ -10,7 +10,7 @@ void main()
 	}
 	for(i=0;i<c+1;i++)
 	{
-		m=m*p;
+		n=n*p;
 	}
-	printf("%d",m);
+	printf("%d",n);
 }
