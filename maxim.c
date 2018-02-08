e4c21106e1a54a27ef667cf77d338593026bf186
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-02-08 16:23:27 +0530

    Create maxim.c

---
+++ maxim.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(void)
+{
+int i,n,temp,a[20],max;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=o;j<n;j++)
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("%d",a[0]);
+return 0;
+}
