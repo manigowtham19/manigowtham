53af0ecf50a167dafc8e4d439ae2eb758220d251
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-02-08 15:56:24 +0530

    Rename numb.c to numg.c

---
+++ numg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  if(a%2!=0)
+  {
+    printf(" the nearest even number is: %d",a-1);
+  }
+  else
+  {
+    printf("nearest even number is: %d",a-2);
+  }
+  }
