d3a3d22597ee708a28358971b51d5d405cfcc91d
manigowtham19 <35257833+manigowtham19@users.noreply.github.com>
2018-02-27 15:35:45 +0530

    Create nl.py

---
+++ nl.py
@@ -0,0 +1,11 @@
+n=int(input("Enter N value:"))
+l=int(input("Enter L value:"))
+r=int(input("Enter R value:"))
+flag=0
+for x in range(l,r+1):
+	if x==n:
+		flag=1
+if(flag==1):
+	print("N ")
+else:
+	print("NO N")
